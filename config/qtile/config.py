7b2e16822c5228468567a1562fb806b6314c1bb7
dondutra <master.dondutra@gmail.com>
2025-08-12 21:33:52 +0200

    lock session keybinding qtile added

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -73,6 +73,7 @@ keys = [
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod], "l", lazy.spawn("dm-tool lock"), desc="Lock the session"),
 ]
 
 # Add key bindings to switch VTs in Wayland.
